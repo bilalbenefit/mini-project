6a66178dfc47aa640e80547b64305b8202c76665
Bilal <bilalbenefit30@gmail.com>
2024-05-12 15:42:12 +0700

    staging

--- my_project/models/staging/stg_sales.sql
+++ my_project/models/staging/stg_sales.sql
@@ -1,11 +1,11 @@
 select
-    order_number::number as order_number
-    , line_item::number as line_item
+    order_number::int as order_number
+    , line_item::int as line_item
     , order_date::date as order_date
     , delivery_date::date as delivery_date
-    , customerkey::number as customer_id
-    , storekey::number as store_id
-    , productkey::number as product_id
-    , quantity::number as order_qty
+    , customerkey::int as customer_id
+    , storekey::int as store_id
+    , productkey::int as product_id
+    , quantity::int as order_qty
     , currency_code as order_currency_code
 from {{ source('raw', 'sales') }}
